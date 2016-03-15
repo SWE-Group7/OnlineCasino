058ae606208a8b17eb72d3bed84821ac5bf748cb
Hayden Thrasher <haydenthrash@gmail.com>
2016-03-15 01:46:09 -0500

    Changed the Intermediate class some. Should be better now.

---
+++ OnlineCasino/Intermediate/Communications/BlackjackServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedModels.GameComponents;
+
+namespace Intermediate.Communications
+{
+    class BlackjackServer : Server
+    {
+        public void DealCards(Card card, int seatNumber)
+        {
+            //Sends card from server to client with specific seat number
+        }
+    }
+}
---
+++ OnlineCasino/Intermediate/Communications/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedModels.GameComponents;
+
+namespace Intermediate.Communications
+{
+    public class Client
+    {
+        public void OpenConnection(/*connection params*/)
+        {
+
+        }
+
+        public void ChooseGame()
+        {
+
+        }
+
+        public void EndGame(bool endGame /*connection stuff*/)
+        {
+            //ends game and terminates connection
+        }
+    }
+}
