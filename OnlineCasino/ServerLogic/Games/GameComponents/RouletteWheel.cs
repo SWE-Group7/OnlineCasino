ff4ef237ac311c3d4031e42c5c72d502f280631a
Hayden Thrasher <haydenthrash@gmail.com>
2016-04-05 18:39:03 -0500

    RouletteWheel class

---
+++ OnlineCasino/ServerLogic/Games/GameComponents/RouletteWheel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerLogic.Games.GameComponents
+{
+    public class RouletteWheel
+    {
+        private int MAX_NUMBER = 36;
+
+        public RouletteWheel()
+        {
+
+        }
+
+        public int Spin()
+        {
+            int spunNum;
+
+            Random spin = new Random();
+            spunNum = spin.Next(MAX_NUMBER, 0);
+
+            return spunNum;
+        }
+
+        public String getColor (int number)
+        {
+            if (number == 0)
+                return "green";
+
+            else if (number % 2 == 0)
+                return "black";
+            else
+                return "red";
+        }
+    }
+}
