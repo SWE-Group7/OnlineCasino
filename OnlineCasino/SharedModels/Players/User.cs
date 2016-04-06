d1e17e7bc4f952486009f712d81405029f4016d8
ndevich <devichnadine@gmail.com>
2016-04-05 22:17:45 -0500

    Added user class to sharedmodels

---
+++ OnlineCasino/SharedModels/Players/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedModels.Players
+{
+    public class User
+    {
+        readonly public int UserID;
+        readonly public string Username;
+        readonly public string FullName;
+        readonly public string EmailAddress;
+        readonly private string HashedPassword;
+        readonly private string Salt;
+        public bool InGame;
+
+        private User(int uID, string username, string fullname, string email)
+        {
+            // user sign in
+        }
+
+        public static bool Register(string username, string password, string email, string fullName)
+        {
+            // user registration (false if unable to register)
+            return false;
+        }
+    }
+}
